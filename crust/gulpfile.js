e4ad437b13711be6e6b11d42a7b909c44f3f8138
marvindanig <marvin@bubbl.in>
2023-02-09 14:15:10 -0500

    Bringing Bookiza upto date with Node 18 as moduleType with javascript imports instead of require.

--- crust/gulpfile.js
+++ crust/gulpfile.js
@@ -1,245 +1,247 @@
-(((r) => {
-  // *************************************//
-  // *********** DEPENDENCIES ************//
-  // *************************************//
+// *************************************//
+// *********** DEPENDENCIES ************//
+// *************************************//
 
-  const gulp = r('gulp')
-  const fse = r('fs-extra')
-  const fs = r('fs')
-  const path = r('path')
-  const browserSync = r('browser-sync')
-  const handlebars = r('gulp-compile-handlebars')
-  const concat = r('gulp-concat')
-  const gulpif = r('gulp-if')
-  const book = r('book-length')
-  const del = r('del')
-  const delayed = r('delayed')
+import gulp from 'gulp'
+import fse from 'fs-extra'
+import fs from 'fs'
+import path from 'path'
+import browserSync from 'browser-sync'
+import handlebars from 'gulp-compile-handlebars'
+import concat from 'gulp-concat'
+import gulpif from 'gulp-if'
+import book from 'book-length'
+import {deleteAsync} from 'del'
+import delayed from 'delayed'
 
-  // Preprocessors / Transpilers
-  const haml = r('gulp-haml')
-  const markdown = r('gulp-markdown')
-  const sass = r('gulp-sass')
-  const less = r('gulp-less')
-  const stylus = r('gulp-stylus')
-  // const postcss = r('gulp-postcss')
+// Preprocessors / Transpilers
+import haml from 'gulp-haml'
+import markdown from 'gulp-markdown'
+import nodeSass from "node-sass"
+import gulpSass from 'gulp-sass' //(require('node-sass'))
+import less from 'gulp-less'
+import stylus from 'gulp-stylus'
 
-  // *************************************//
-  // ************ Build BUILD ************//
-  // *************************************//
+const sass = gulpSass(nodeSass)
 
-  function getFolders (dir) {
-    return fs.readdirSync(dir)
-      .filter(file => fs.statSync(path.join(dir, file)).isDirectory())
-  }
+// const postcss from 'gulp-postcss'
 
-  gulp.task('pages', (done) => {
-    return gulp.src(path.join('manuscript', '*', '*'))
-      .pipe(gulpif(/[.]haml$/, haml()))
-      .pipe(gulpif(/[.]md$/, markdown()))
-      .pipe(gulpif(/[.]markdown$/, markdown()))
-      .pipe(gulpif(/[.]scss|sass$/, sass()))
-      .pipe(gulpif(/[.]less$/, less()))
-      .pipe(gulpif(/[.]styl$/, stylus()))
-      .pipe(gulp.dest(path.join('build', 'manuscript')))
+// *************************************//
+// ************ Build BUILD ************//
+// *************************************//
 
-    done()
-  })
+function getFolders (dir) {
+  return fs.readdirSync(dir)
+    .filter(file => fs.statSync(path.join(dir, file)).isDirectory())
+}
 
-  gulp.task('templates', (done) => {
-    return gulp.src(path.join('templates', '*.*'))
-      .pipe(gulpif(/[.]haml$/, haml()))
-      .pipe(gulpif(/[.]md$/, markdown()))
-      .pipe(gulpif(/[.]markdown$/, markdown()))
-      .pipe(gulpif(/[.]scss|sass$/, sass()))
-      .pipe(gulpif(/[.]less$/, less()))
-      .pipe(gulpif(/[.]styl$/, stylus()))
-      .pipe(gulp.dest(path.join('build', 'templates')))
-    done()
-  })
+gulp.task('pages', (done) => {
+  return gulp.src(path.join('manuscript', '*', '*'))
+    .pipe(gulpif(/[.]haml$/, haml()))
+    .pipe(gulpif(/[.]md$/, markdown()))
+    .pipe(gulpif(/[.]markdown$/, markdown()))
+    .pipe(gulpif(/[.]scss|sass$/, sass()))
+    .pipe(gulpif(/[.]less$/, less()))
+    .pipe(gulpif(/[.]styl$/, stylus()))
+    .pipe(gulp.dest(path.join('build', 'manuscript')))
 
-  gulp.task('renderBook', gulp.series('pages', 'templates', (done) => {
-    const folders = getFolders(path.join('.', 'build', 'manuscript'))
-    folders.map(folder => { renderPage(folder) })
+  done()
+})
 
-    done()
-  }))
+gulp.task('templates', (done) => {
+  return gulp.src(path.join('templates', '*.*'))
+    .pipe(gulpif(/[.]haml$/, haml()))
+    .pipe(gulpif(/[.]md$/, markdown()))
+    .pipe(gulpif(/[.]markdown$/, markdown()))
+    .pipe(gulpif(/[.]scss|sass$/, sass()))
+    .pipe(gulpif(/[.]less$/, less()))
+    .pipe(gulpif(/[.]styl$/, stylus()))
+    .pipe(gulp.dest(path.join('build', 'templates')))
+  done()
+})
 
-  // *************************************//
-  // ************ Book Indexer ***********//
-  // *************************************//
+gulp.task('renderBook', gulp.series('pages', 'templates', (done) => {
+  const folders = getFolders(path.join('.', 'build', 'manuscript'))
+  folders.map(folder => { renderPage(folder) })
 
-  gulp.task('indexPage', (done) => {
-    indexPageHandler()
+  done()
+}))
 
-    done()
-  })
+// *************************************//
+// ************ Book Indexer ***********//
+// *************************************//
 
-  function indexPageHandler () {
-    const bookLength = book.length()
+gulp.task('indexPage', (done) => {
+  indexPageHandler()
 
-    // let contentString = ''
+  done()
+})
 
-    // for (let index = 1; index <= bookLength; index++) {
-    //   contentString += `<div class='page'><iframe src='build/renders/page-${index}.html'></iframe></div>`
-    // }
+function indexPageHandler () {
+  const bookLength = book.length()
 
-    fse.readJson(path.join('.', '.bookrc')).then((json) => {
-      return {
-        BOOKNAME: json.name,
-        BOOKLENGTH: bookLength
-      }
-    }).then((templateData) => {
-      gulp.src(path.join('.', 'crust', 'index-template.html'))
-        .pipe(handlebars(templateData, {}))
-        .pipe(concat('index.html'))
-        .pipe(gulp.dest('.'))
-        .pipe(browserSync.stream())
-    }).catch((err) => {
-      console.log('Something went wrong', err)
-    })
-  }
+  // let contentString = ''
 
-  // *************************************//
-  // ************ Page Renderer **********//
-  // *************************************//
+  // for (let index = 1; index <= bookLength; index++) {
+  //   contentString += `<div class='page'><iframe src='build/renders/page-${index}.html'></iframe></div>`
+  // }
 
-  function renderPage (page) {
-    const bodyPath = path.join('.', 'build', 'manuscript', page, 'body.html')
-    const headPath = path.join('.', 'build', 'manuscript', page, 'head.html')
-    const scriptPath = path.join('.', 'build', 'manuscript', page, 'script.js')
-    const stylePath = path.join('.', 'build', 'manuscript', page, 'style.css')
-    const templateStylePath = path.join('.', 'build', 'templates', 'style.css')
-		const templateHeadPath = path.join('.', 'build', 'templates', 'head.html')
-		const templateScriptPath = path.join('.', 'build', 'templates', 'script.js')
-
-    let bodyContent = ''
-    let styleContent = ''
-    let scriptContent = ''
-    let headContent = ''
-    let templateHeadContent = ''
-		let templateScriptContent = ''
-    let templateStyleContent = ''
-
-		// TODO: Use promises here?
-
-    if (fs.existsSync(bodyPath)) {
-      bodyContent = fs.readFileSync(bodyPath, 'utf-8').toString()
+  fse.readJson(path.join('.', '.bookrc')).then((json) => {
+    return {
+      BOOKNAME: json.name,
+      BOOKLENGTH: bookLength
     }
-    if (fs.existsSync(stylePath)) {
-      styleContent = fs.readFileSync(stylePath, 'utf-8').toString()
-    }
-    if (fs.existsSync(templateStylePath)) {
-      templateStyleContent = fs.readFileSync(templateStylePath, 'utf-8').toString()
-    }
-
-    if (fs.existsSync(headPath)) {
-      headContent = fs.readFileSync(headPath, 'utf-8').toString()
-    }
-    if (fs.existsSync(templateHeadPath)) {
-      templateHeadContent = fs.readFileSync(templateHeadPath, 'utf-8').toString()
-    }
-    if (fs.existsSync(scriptPath)) {
-      scriptContent = fs.readFileSync(scriptPath, 'utf-8').toString()
-		}
-
-    if (fs.existsSync(templateScriptPath)) {
-      templateScriptContent = fs.readFileSync(templateScriptPath, 'utf-8').toString()
-    }
-
-
-    const pageTemplateData = { bodyContent, templateStyleContent, styleContent, headContent, templateHeadContent, scriptContent, templateScriptContent }
-
-    gulp.src(path.join('.', 'crust', 'page-template.html'))
-      .pipe(handlebars(pageTemplateData, {}))
-      .pipe(concat(`${page}.html`))
-      .pipe(gulp.dest(path.join('.', 'build', 'renders')))
+  }).then((templateData) => {
+    gulp.src(path.join('.', 'crust', 'index-template.html'))
+      .pipe(handlebars(templateData, {}))
+      .pipe(concat('index.html'))
+      .pipe(gulp.dest('.'))
       .pipe(browserSync.stream())
+  }).catch((err) => {
+    console.log('Something went wrong', err)
+  })
+}
+
+// *************************************//
+// ************ Page Renderer **********//
+// *************************************//
+
+function renderPage (page) {
+  const bodyPath = path.join('.', 'build', 'manuscript', page, 'body.html')
+  const headPath = path.join('.', 'build', 'manuscript', page, 'head.html')
+  const scriptPath = path.join('.', 'build', 'manuscript', page, 'script.js')
+  const stylePath = path.join('.', 'build', 'manuscript', page, 'style.css')
+  const templateStylePath = path.join('.', 'build', 'templates', 'style.css')
+  const templateHeadPath = path.join('.', 'build', 'templates', 'head.html')
+  const templateScriptPath = path.join('.', 'build', 'templates', 'script.js')
+
+  let bodyContent = ''
+  let styleContent = ''
+  let scriptContent = ''
+  let headContent = ''
+  let templateHeadContent = ''
+  let templateScriptContent = ''
+  let templateStyleContent = ''
+
+  // TODO: Use promises here?
+
+  if (fs.existsSync(bodyPath)) {
+    bodyContent = fs.readFileSync(bodyPath, 'utf-8').toString()
+  }
+  if (fs.existsSync(stylePath)) {
+    styleContent = fs.readFileSync(stylePath, 'utf-8').toString()
+  }
+  if (fs.existsSync(templateStylePath)) {
+    templateStyleContent = fs.readFileSync(templateStylePath, 'utf-8').toString()
   }
 
-  // Glob pattern matching
-  var glob = [path.join('manuscript', '*'),
-    path.join('manuscript', '*', '*.+(js|css|html|markdown|md|haml|less|styl|scss|sass)')
-  ]
+  if (fs.existsSync(headPath)) {
+    headContent = fs.readFileSync(headPath, 'utf-8').toString()
+  }
+  if (fs.existsSync(templateHeadPath)) {
+    templateHeadContent = fs.readFileSync(templateHeadPath, 'utf-8').toString()
+  }
+  if (fs.existsSync(scriptPath)) {
+    scriptContent = fs.readFileSync(scriptPath, 'utf-8').toString()
+  }
 
-  // *************************************//
-  // ************ Page Renderer **********//
-  // *************************************//
+  if (fs.existsSync(templateScriptPath)) {
+    templateScriptContent = fs.readFileSync(templateScriptPath, 'utf-8').toString()
+  }
 
-  gulp.task('watchBook', function () {
-    browserSync.init({
-      server: './',
-      port: 4567,
-      notify: false,
-      logLevel: 'debug'
-    })
 
-    var trashWatcher = gulp.watch(path.join('trash', '*'), gulp.series('indexPage'))
+  const pageTemplateData = { bodyContent, templateStyleContent, styleContent, headContent, templateHeadContent, scriptContent, templateScriptContent }
 
-    trashWatcher.on('add', function (pagePath, stats) {
-      const paths = pagePath.split(path.sep)
-      let page = paths[paths.length - 1] === '' ? paths[paths.length - 2] : paths[paths.length - 1]
-      page = `${page.split('-')[0]}-${page.split('-')[1]}`
-      del(path.join('build', 'manuscript', page))
-      del(path.join('build', 'renders', `${page}.html`))
-    })
+  gulp.src(path.join('.', 'crust', 'page-template.html'))
+    .pipe(handlebars(pageTemplateData, {}))
+    .pipe(concat(`${page}.html`))
+    .pipe(gulp.dest(path.join('.', 'build', 'renders')))
+    .pipe(browserSync.stream())
+}
 
-    gulp.watch(path.join('templates', '**.*'), gulp.series('renderBook'))
+// Glob pattern matching
+var glob = [path.join('manuscript', '*'),
+  path.join('manuscript', '*', '*.+(js|css|html|markdown|md|haml|less|styl|scss|sass)')
+]
 
-    var globWatcher = gulp.watch(glob, gulp.series('renderBook', 'indexPage'))
+// *************************************//
+// ************ Page Renderer **********//
+// *************************************//
 
-    globWatcher.on('change', function(pagePath, stats){
-      const paths = pagePath.split(path.sep)
-      if (paths[paths.length - 1] === '') {
-        var page = paths[paths.length - 2]
-      } else if (paths[paths.length - 1].split('-')[0] === 'page') {
-        page = paths[paths.length - 1]
-      } else {
-        page = paths[paths.length - 2]
-      }
-    });
-
-    globWatcher.on('add', function (pagePath, stats) {
-      const paths = pagePath.split(path.sep)
-      if (paths[paths.length - 1] === '') {
-        var page = paths[paths.length - 2]
-      } else if (paths[paths.length - 1].split('-')[0] === 'page') {
-        page = paths[paths.length - 1]
-      } else {
-        page = paths[paths.length - 2]
-        pagePath = path.dirname(obj.path)
-      }
-
-      del(path.join('build', 'manuscript', page))
-
-      let delay = 1000
-
-      delayed.delay(() => {
-        const stats = fs.statSync(pagePath)
-        if (stats.isDirectory()) {
-          gulp.src(path.join(pagePath, '*'))
-            .pipe(gulpif(/[.]haml$/, haml()))
-            .pipe(gulpif(/[.]md$/, markdown()))
-            .pipe(gulpif(/[.]markdown$/, markdown()))
-            .pipe(gulpif(/[.]scss|sass$/, sass()))
-            .pipe(gulpif(/[.]less$/, less()))
-            .pipe(gulpif(/[.]styl$/, stylus()))
-            .pipe(gulp.dest(path.join('build', 'manuscript', page)))
-            .on('end', () => {
-              renderPage(page)
-            })
-        }
-      }, delay)
-    })
+gulp.task('watchBook', function () {
+  browserSync.init({
+    server: './',
+    port: 4567,
+    notify: false,
+    logLevel: 'debug'
   })
 
-  // *************************************//
-  // ************ DEFAULT TASK ***********//
-  // *************************************//
+  var trashWatcher = gulp.watch(path.join('trash', '*'), gulp.series('indexPage'))
 
-  gulp.task('default', gulp.series('renderBook', 'indexPage', 'watchBook',
-    function (done) {
-      done()
+  trashWatcher.on('add', function (pagePath, stats) {
+    const paths = pagePath.split(path.sep)
+    let page = paths[paths.length - 1] === '' ? paths[paths.length - 2] : paths[paths.length - 1]
+    page = `${page.split('-')[0]}-${page.split('-')[1]}`
+    deleteAsync(path.join('build', 'manuscript', page))
+    deleteAsync(path.join('build', 'renders', `${page}.html`))
+  })
+
+  gulp.watch(path.join('templates', '**.*'), gulp.series('renderBook'))
+
+  var globWatcher = gulp.watch(glob, gulp.series('renderBook', 'indexPage'))
+
+  globWatcher.on('change', function(pagePath, stats){
+    const paths = pagePath.split(path.sep)
+    if (paths[paths.length - 1] === '') {
+      var page = paths[paths.length - 2]
+    } else if (paths[paths.length - 1].split('-')[0] === 'page') {
+      page = paths[paths.length - 1]
+    } else {
+      page = paths[paths.length - 2]
+    }
+  });
+
+  globWatcher.on('add', function (pagePath, stats) {
+    const paths = pagePath.split(path.sep)
+    if (paths[paths.length - 1] === '') {
+      var page = paths[paths.length - 2]
+    } else if (paths[paths.length - 1].split('-')[0] === 'page') {
+      page = paths[paths.length - 1]
+    } else {
+      page = paths[paths.length - 2]
+      pagePath = path.dirname(obj.path)
     }
 
-  ))
-}))(require)
+    deleteAsync(path.join('build', 'manuscript', page))
+
+    let delay = 1000
+
+    delayed.delay(() => {
+      const stats = fs.statSync(pagePath)
+      if (stats.isDirectory()) {
+        gulp.src(path.join(pagePath, '*'))
+          .pipe(gulpif(/[.]haml$/, haml()))
+          .pipe(gulpif(/[.]md$/, markdown()))
+          .pipe(gulpif(/[.]markdown$/, markdown()))
+          .pipe(gulpif(/[.]scss|sass$/, sass()))
+          .pipe(gulpif(/[.]less$/, less()))
+          .pipe(gulpif(/[.]styl$/, stylus()))
+          .pipe(gulp.dest(path.join('build', 'manuscript', page)))
+          .on('end', () => {
+            renderPage(page)
+          })
+      }
+    }, delay)
+  })
+})
+
+// *************************************//
+// ************ DEFAULT TASK ***********//
+// *************************************//
+
+gulp.task('default', gulp.series('renderBook', 'indexPage', 'watchBook',
+  function (done) {
+    done()
+  }
+
+))
--- scripts/displayLicense.js
+++ scripts/displayLicense.js
@@ -1,32 +1,27 @@
-((r) => {
-  'use strong'
+import fse from 'fs-extra'
+import chalk from 'chalk'
+import cl from 'chalkline'
+import terminalLink from 'terminal-link'
+// const center = require('center-align')
 
-  const fse = r('fs-extra')
-  const chalk = r('chalk')
-  const cl = require('chalkline')
-  const terminalLink = require('terminal-link')
-  const center = require('center-align')
+const docLink = terminalLink('docs@bookiza', 'https://www.bookiza.io/docs/blueoak')
+const supportLink = terminalLink('support@bookiza', 'https://git.io/fj67o')
 
+cl.green()
+cl.blue()
+cl.red()
 
-  const docLink = terminalLink('docs@bookiza', 'https://www.bookiza.io/docs/blueoak')
-  const supportLink = terminalLink('support@bookiza', 'https://git.io/fj67o')
+fse.readFile('./LICENSE.markdown', 'utf8', (err, data) => {
+  if (err) return Error("Couldn't read license information.")
+  console.log(chalk.magenta(data))
+  console.log(chalk.white.bold(` Copyright © 3rd millenium+ Marvin Danig, Sonica Arora and Bubblin Superbooks. \n`))
+  cl.white()
+  console.log('\n Installation is complete. Yay! 🍾')
+  // console.log(`\n Welcome to Bookiza Abelone.`)
+  console.log(`\n Next step: Run ${chalk.bold.yellow('$ bookiza register')} and to connect with Bubblin. \n`)
+  console.log(` Visit: ${chalk.yellowBright(docLink)} for more information.`)
+  console.log(` Support: ${chalk.yellowBright(supportLink)}\n`)
+  cl.white()
 
-  cl.green()
-  cl.blue()
-  cl.red()
-
-  fse.readFile('./LICENSE.markdown', 'utf8', (err, data) => {
-    if (err) return Error('Couldn\'t read license information.')
-    console.log(chalk.keyword('magenta')(data))
-    console.log(chalk.bold.white(` Copyright © 3rd millenium+ Marvin Danig, Sonica Arora and Bubblin Superbooks. \n`))
-    cl.white()
-    console.log('\n Installation is complete. Yay! 🍾')
-    // console.log(`\n Welcome to Bookiza Abelone.`)
-    console.log(`\n Next step: Run ${chalk.bold.yellow('$ bookiza register')} and to connect with Bubblin. \n`)
-    console.log(` Visit: ${chalk.keyword('orange')(docLink)} for more information.`)
-    console.log(` Support: ${chalk.keyword('orange')(supportLink)}\n`)
-    cl.white()
-
-    console.log('\n\n\n')
-  })
-})(require)
+  console.log('\n\n\n')
+})
